fdc5f6281af104fa1485bd69d92a468e2321aa36
Joel Kwiecinski <jk.novaproducciones@gmail.com>
2022-11-03 17:05:08 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoPruebaStatic/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPruebaStatic
+{
+    public class Empleado
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        private static int comision;
+
+        public static int Comision
+        {
+            get { return comision; }
+            set { comision = value; }
+        }
+
+
+        public Empleado(int id, string nombre, string apellido)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+
+        }
+    }
+}
---
+++ WindowsPresentacion/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProyectoPruebaStatic;
+
+namespace WindowsPresentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComision_Click(object sender, EventArgs e)
+        {
+            Empleado empleado1 = new Empleado(0, "Juan", "Perez");
+
+            MessageBox.Show("Comisión: " + Empleado.Comision);
+
+            Empleado empleado2 = new Empleado(1, "Lucas", "Gonzalez");
+            Empleado.Comision = 2030;
+
+            MessageBox.Show("Comisión: " + Empleado.Comision);
+
+            Empleado empleado3 = new Empleado(2, "Andy", "Gallardo");
+            Empleado.Comision = 530;
+
+            MessageBox.Show("Comisión: " + Empleado.Comision);
+
+            Empleado empleado4 = new Empleado(1, "Franco", "Gimenez");
+            Empleado.Comision = 5728;
+
+            MessageBox.Show("Comisión: " + Empleado.Comision);
+
+        }
+    }
+}
